9f097c5a1ac0e07f1ae705ac044780776bde66ad
erqsor <ant.lafarge@gmail.com>
2011-09-27 02:11:13 +0200

    Base implementation (opening handshake)

---
+++ QtWebSocket/QWsServer.cpp
@@ -0,0 +1,184 @@
+#include "QWsServer.h"
+
+#include <QTcpSocket>
+#include <QRegExp>
+#include <QStringList>
+#include <QString>
+#include <QByteArray>
+#include <QCryptographicHash>
+
+const QString QWsServer::regExpResourceNameStr( "GET\\s(.*)\\sHTTP/1.1\r\n");
+const QString QWsServer::regExpHostStr( "Host:\\s(.+:\\d+)\r\n" );
+const QString QWsServer::regExpKeyStr( "Sec-WebSocket-Key:\\s(.{24})\r\n" );
+const QString QWsServer::regExpVersionStr( "Sec-WebSocket-Version:\\s(\\d)\r\n" );
+const QString QWsServer::regExpOriginStr( "Sec-WebSocket-Origin:\\s(.+)\r\n" );
+const QString QWsServer::regExpProtocolStr( "Sec-WebSocket-Protocol:\\s(.+)\r\n" );
+const QString QWsServer::regExpExtensionsStr( "Sec-WebSocket-Extensions:\\s(.+)\r\n" );
+
+QWsServer::QWsServer(QObject * parent)
+	: QTcpServer(parent)
+{
+    tcpServer = new QTcpServer(this);
+	connect(tcpServer, SIGNAL(newConnection()), this, SLOT(newTcpConnection()));
+}
+
+QWsServer::~QWsServer()
+{
+	tcpServer->deleteLater();
+}
+
+bool QWsServer::listen(const QHostAddress & address, quint16 port)
+{
+	bool launched = tcpServer->listen(address, port);
+
+	if ( ! launched )
+		treatSocketError();
+
+	return launched;
+}
+
+void QWsServer::close()
+{
+	tcpServer->close();
+}
+
+void QWsServer::treatSocketError()
+{
+	serverSocketError = tcpServer->serverError();
+	serverSocketErrorString = tcpServer->errorString();
+}
+
+QAbstractSocket::SocketError QWsServer::serverError()
+{
+	return serverSocketError;
+}
+
+QString QWsServer::errorString()
+{
+	return serverSocketErrorString;
+}
+
+void QWsServer::newTcpConnection()
+{
+    QTcpSocket * clientSocket = tcpServer->nextPendingConnection();
+
+	QObject * clientObject = qobject_cast<QObject*>(clientSocket);
+
+    connect(clientObject, SIGNAL(readyRead()), this, SLOT(dataReceived()));
+    connect(clientObject, SIGNAL(disconnected()), this, SLOT(clientDisconnected()));
+}
+
+void QWsServer::clientDisconnected()
+{
+    QTcpSocket * clientSocket = qobject_cast<QTcpSocket*>(sender());
+    if (clientSocket == 0)
+        return;
+}
+
+void QWsServer::dataReceived()
+{
+    QTcpSocket * clientSocket = qobject_cast<QTcpSocket*>(sender());
+    if (clientSocket == 0)
+        return;
+
+	QString request( clientSocket->readAll() );
+
+	QRegExp regExp;
+	regExp.setMinimal( true );
+	
+	// Extract mandatory datas
+	// Resource name
+	regExp.setPattern( QWsServer::regExpResourceNameStr );
+	regExp.indexIn(request);
+	resourceName = regExp.cap(1);
+	
+	// Host (address & port)
+	regExp.setPattern( QWsServer::regExpHostStr );
+	regExp.indexIn(request);
+	QStringList sl = regExp.cap(1).split(':');
+	QString hostPort;
+	if ( sl.size() > 1 )
+		hostPort = sl[1];
+	QString hostAddress = sl[0];
+	
+	// Key
+	regExp.setPattern( QWsServer::regExpKeyStr );
+	regExp.indexIn(request);
+	QString key = regExp.cap(1);
+	
+	// Version
+	regExp.setPattern( QWsServer::regExpVersionStr );
+	regExp.indexIn(request);
+	QString version = regExp.cap(1);
+	
+	// Extract optional datas
+	// Origin
+	regExp.setPattern( QWsServer::regExpOriginStr );
+	regExp.indexIn(request);
+	QString origin = regExp.cap(1);
+
+	regExp.setPattern( QWsServer::regExpProtocolStr );
+	regExp.indexIn(request);
+	QString protocol = regExp.cap(1);
+
+	regExp.setPattern( QWsServer::regExpExtensionsStr );
+	regExp.indexIn(request);
+	QString extensions = regExp.cap(1);
+
+	// If the mandatory params are not setted, we abord the handshake
+	if ( hostAddress.isEmpty()
+		|| hostPort.isEmpty()
+		|| resourceName.isEmpty()
+		|| key.isEmpty()
+		|| version != "8" )
+		return;
+
+	incomingConnection( clientSocket->socketDescriptor() );
+
+	// Compose answer
+	QString accept = computeAcceptV8( key );
+	
+	QString answer("HTTP/1.1 101 Switching Protocols\r\n");
+	answer.append("Upgrade: websocket\r\n");
+	answer.append("Connection: Upgrade\r\n");
+	answer.append("Sec-WebSocket-Accept: " + accept + "\r\n");
+	answer.append("\r\n");
+
+	clientSocket->write( answer.toUtf8() );
+}
+
+QString QWsServer::computeAcceptV8(QString key)
+{
+	key += "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+	QByteArray hash = QCryptographicHash::hash ( key.toUtf8(), QCryptographicHash::Sha1 );
+	return hash.toBase64();
+}
+
+void QWsServer::addPendingConnection( QTcpSocket * socket )
+{
+	if ( pendingConnections.size() < QTcpServer::maxPendingConnections() )
+		pendingConnections.enqueue(socket);
+}
+
+void QWsServer::incomingConnection( int socketDescriptor )
+{
+	QTcpSocket * socket = new QTcpSocket(tcpServer);
+	socket->setSocketDescriptor( socketDescriptor );
+	
+	addPendingConnection( socket );
+
+	emit QWsServer::newConnection();
+}
+
+bool QWsServer::hasPendingConnections()
+{
+	if ( pendingConnections.size() > 0 )
+		return true;
+	return false;
+}
+
+QTcpSocket * QWsServer::nextPendingConnection()
+{
+	return pendingConnections.dequeue();
+}
+
---
+++ QtWebSocket/QWsServer.h
@@ -0,0 +1,65 @@
+#ifndef QWSSERVER_H
+#define QWSSERVER_H
+
+#include <QTcpServer>
+#include <QString>
+#include <QQueue>
+
+class QWsServer : public QTcpServer
+{
+	Q_OBJECT
+
+public:
+	// ctor
+	QWsServer(QObject * parent = 0);
+	// dtor
+	virtual ~QWsServer();
+
+	// public methods
+	bool listen(const QHostAddress & address = QHostAddress::Any, quint16 port = 0);
+	void close();
+	QAbstractSocket::SocketError serverError();
+	QString errorString();
+	bool hasPendingConnections();
+	virtual QTcpSocket * nextPendingConnection();
+
+protected:
+	// Protected methods
+	void addPendingConnection( QTcpSocket * socket );
+	void incomingConnection( int socketDescriptor );
+
+signals:
+	// signals
+	void newConnection();
+
+private:
+	// private methods
+	void treatSocketError();
+	QString computeAcceptV8(QString key);
+
+private slots:
+	// private slots
+	void newTcpConnection();
+	void dataReceived();
+	void clientDisconnected();
+
+private:
+	// private attributes
+    QTcpServer * tcpServer;
+    QString resourceName;
+	QAbstractSocket::SocketError serverSocketError;
+	QString serverSocketErrorString;
+	QQueue<QTcpSocket*> pendingConnections;
+
+public:
+	// public static vars
+	static const QString regExpResourceNameStr;
+	static const QString regExpHostStr;
+	static const QString regExpKeyStr;
+	static const QString regExpVersionStr;
+	static const QString regExpOriginStr;
+	static const QString regExpProtocolStr;
+	static const QString regExpExtensionsStr;
+};
+
+#endif // QWSSERVER_H
