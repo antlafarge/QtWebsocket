3835f1055ff48f7893ccaa04633f919e081f8790
Antoine Lafarge <ant.lafarge@gmail.com>
2013-08-24 13:45:45 +0200

    client can now connect to echo.websocket.org

--- QtWebsocket/QWsSocket.cpp
+++ QtWebsocket/QWsSocket.cpp
@@ -83,18 +83,19 @@ void QWsSocket::connectToHost(const QString& hostName, quint16 port, OpenMode mo
 {
 	_hostPort = port;
 	QString hostName2 = QString(hostName).remove("ws://", Qt::CaseInsensitive);
-	if (hostName2.compare(QLatin1String("localhost"), Qt::CaseInsensitive))
+	if (hostName2.contains(QRegExp("^localhost$", Qt::CaseInsensitive)))
 	{
 		_host = QLatin1String("localhost");
 		_hostAddress = QHostAddress::LocalHost;
 	}
 	else if (hostName2.contains(QRegExp(QWsSocket::regExpIPv4)))
 	{
-		_host = QLatin1String("localhost");
+		_host = hostName2;
 		_hostAddress = QHostAddress(hostName2);
 	}
 	else
 	{
+		_host = hostName2;
 		QHostInfo info = QHostInfo::fromName(hostName2);
 		QList<QHostAddress> hostAddresses = info.addresses();
 		_hostAddress = hostAddresses[0];
